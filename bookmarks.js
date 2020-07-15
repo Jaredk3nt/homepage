f4671314897388cd7c15e085b9ffbf9dbef62654
Jared Jones <Jared.Jones@cdk.com>
2020-07-15 14:42:24 -0500

    Updating to store bookmark definitions in js to make them easier to update, formatting, fixing #7

---
+++ bookmarks.js
@@ -0,0 +1,47 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Inbox", url: "https://inbox.google.com" },
+      { name: "GitHub", url: "https://github.com" },
+      { name: "Drive", url: "https://drive.google.com" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Netflix", url: "https://netflix.com" },
+      { name: "Crunchyroll", url: "https://crunchyroll.com" },
+      {
+        name: "Amazon Prime",
+        url: "https://www.amazon.com/Amazon-Video",
+      },
+    ],
+  },
+  {
+    title: "Reddit",
+    links: [
+      { name: "/r/overwatch", url: "https://reddit.com/r/overwatch" },
+      {
+        name: "/r/pcmasterrace",
+        url: "https://reddit.com/r/pcmasterrace",
+      },
+      { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
+      {
+        name: "/r/battlestations",
+        url: "https://reddit.com/r/battlestations",
+      },
+      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
+      { name: "/r/news", url: "https://reddit.com/r/news" },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { name: "Twitter", url: "https://twitter.com" },
+      { name: "Facebook", url: "https://facebook.com" },
+    ],
+  },
+];
